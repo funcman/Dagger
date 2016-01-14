ee0b8353486426a40bc0724fefeb48e4c7a0329b
funcman <hyq1986@gmail.com>
2016-01-14 15:04:58 +0800

    add clipper

--- Engine/Canvas.cpp
+++ Engine/Canvas.cpp
@@ -22,3 +22,46 @@ Canvas::Canvas(int w, int h, void* scr) {
 Canvas::~Canvas() {
     GpCanvas = 0;
 }
+
+bool Canvas::makeClipper(Clipper* clipperPtr) {
+    int xx = clipperPtr->x;
+    int yy = clipperPtr->y;
+    clipperPtr->top     = 0;
+    clipperPtr->left    = 0;
+    clipperPtr->right   = 0;
+
+    if (clipperPtr->y < rect.top) {
+        clipperPtr->y       =   rect.top;
+        clipperPtr->top     =   rect.top - yy;
+        clipperPtr->height  -=  clipperPtr->top;
+    }
+    if (clipperPtr->height <= 0) {
+        return false;
+    }
+
+    if (clipperPtr->height > rect.bottom - clipperPtr->y) {
+        clipperPtr->height = rect.bottom - clipperPtr->y;
+    }
+    if (clipperPtr->height <= 0) {
+        return false;
+    }
+
+    if (clipperPtr->x < rect.left) {
+        clipperPtr->x       =   rect.left;
+        clipperPtr->left    =   rect.left - xx;
+        clipperPtr->width   -=  clipperPtr->left;
+    }
+    if (clipperPtr->width <= 0) {
+        return false;
+    }
+
+    if (clipperPtr->width > rect.right - clipperPtr->x) {
+        clipperPtr->right   =   clipperPtr->width + clipperPtr->x - rect.right;
+        clipperPtr->width   -=  clipperPtr->right;
+    }
+    if (clipperPtr->width <= 0) {
+        return false;
+    }
+
+    return true;
+}
--- Engine/Canvas.h
+++ Engine/Canvas.h
@@ -8,6 +8,17 @@ typedef struct {
     long    bottom;
 } Rect;
 
+typedef struct {
+    long    x;
+    long    y;
+    long    width;
+    long    height;
+    long    left;
+    long    top;
+    long    right;
+    long    bottom;
+} Clipper;
+
 class Canvas {
 public:
     void*       frameBuffer;
@@ -20,10 +31,7 @@ public:
     Canvas(int w, int h, void* scr);
     ~Canvas();
 
-//    void    resize(int width, int height);
-//    bool    isPointInCanvas(int x, int y);
-//    bool    isRectInCanvas(int x, int y, int width, int height);
-//    void    saveToFile(char const* filename);
+    bool    makeClipper(Clipper* clipperPtr);
 };
 
 extern Canvas* GpCanvas;
