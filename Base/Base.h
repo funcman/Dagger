1a6629f08976fa3b125f3048f6cd656306e9692a
funcman <hyq1986@gmail.com>
2026-08-11 07:08:06 +0800

    add md5

--- Base/Base.h
+++ Base/Base.h
@@ -2,6 +2,8 @@
 #define DAGGER_BASE_H
 
 #include "Debug.h"
+#include "List.h"
+#include "Md5.h"
 #include "MemPool.h"
 #include "Memory.h"
 #include "TypeDef.h"
---
+++ Base/Md5.h
@@ -0,0 +1,7 @@
+#ifndef DAGGER_MD5_H
+#define DAGGER_MD5_H
+
+void DMd5(unsigned char* output, unsigned char* input, int len);
+void DMd5String(char* output, char* input);
+
+#endif//DAGGER_MD5_H
